0867a58a24db0fd26fd6323db13321397c5e26ee
Stephen Ward <spworwell@gmail.com>
2016-07-22 23:19:47 -0400

    finished changes to make Treefarm-AC compatible with factorio v0.13

--- control.lua
+++ control.lua
@@ -1,7 +1,14 @@
 
 script.on_event(defines.events.on_built_entity, function(event)
 	if event.created_entity.name == "tf-cokery-dummy" then
-		game.get_surface("nauvis").create_entity{name = "tf-cokery", position = event.created_entity.position, force = game.forces.player}
+		local player = game.players[event.player_index]
+	
+		player.surface.create_entity({
+			name = "tf-cokery", 
+			position = event.created_entity.position, 
+			force = player.force
+		})
+		-- regardless of whether the cookery was created successfully or not, we still want to destroy the placeholder object
 		event.created_entity.destroy()
 	end
 end)
